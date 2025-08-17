261e0fd7ff2befe93846162e6006eb518b6d69fb
Trịnh Hg <onepiecechapter@gmail.com>
2025-08-17 12:09:16 +0700

    Update core.js

--- js/core.js
+++ js/core.js
@@ -6,12 +6,13 @@
 
     // Firebase config
     const firebaseConfig = {
-      apiKey: "YOUR_API_KEY",
-      authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-      projectId: "YOUR_PROJECT_ID",
-      storageBucket: "YOUR_PROJECT_ID.appspot.com",
-      messagingSenderId: "YOUR_SENDER_ID",
-      appId: "YOUR_APP_ID"
+      apiKey: "AIzaSyB2VklwyVqGX7BgIsZeYannPijYk9_bB1Q",
+      authDomain: "trinhhg-1f8f3.firebaseapp.com",
+      projectId: "trinhhg-1f8f3",
+      storageBucket: "trinhhg-1f8f3.firebasestorage.app",
+      messagingSenderId: "63432174844",
+      appId: "1:63432174844:web:57f18e049b4cf5860e7b79"
+      measurementId: "G-LNZQTM2JTD"
     };
 
     // Khởi tạo Firebase
